647e38a2f0ec3aca33da57e80b320351b97c8e60
panittawee <82809044+panittawee@users.noreply.github.com>
2021-05-27 04:51:39 +0700

    Add files via upload

---
+++ act3/ap3.js
@@ -0,0 +1,23 @@
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-nav-toggle");
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
+
+
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++)
+  slides[slideIndex-1].style.display = "block";  
+  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
